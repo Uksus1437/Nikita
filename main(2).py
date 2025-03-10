451f5daa85af5042ac767fcb7adc764495691732
Uksus1437 <116167254+Uksus1437@users.noreply.github.com>
2025-03-10 19:37:38 +0300

    10.03

--- main(2).py
+++ main(2).py
@@ -1,6 +1,6 @@
-ip = list(map(int, '172.17.167.18'.split('.')))
+ip = list(map(int, '93.138.96.47'.split('.')))
 print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfill(8)}.{bin(ip[3])[2:].zfill(8)}')
 
 
-ip = list(map(int, '255.255.240.0'.split('.')))
+ip = list(map(int, '93.138.64.0'.split('.')))
 print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfill(8)}.{bin(ip[3])[2:].zfill(8)}')
