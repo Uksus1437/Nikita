72b4a99e5382301e91c21923246078f31e88ed39
Uksus1437 <116167254+Uksus1437@users.noreply.github.com>
2025-03-24 19:35:16 +0300

    24.03

--- main(2).py
+++ main(2).py
@@ -4,3 +4,5 @@ print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfil
 
 ip = list(map(int, '93.138.64.0'.split('.')))
 print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfill(8)}.{bin(ip[3])[2:].zfill(8)}')
+
+
