6e76a16f96ab805c258f5218b8a66e8ab6f066ed
Uksus1437 <116167254+Uksus1437@users.noreply.github.com>
2025-04-07 19:47:10 +0300

    07.04

--- main(2).py
+++
@@ -1,8 +0,0 @@
-ip = list(map(int, '93.138.96.47'.split('.')))
-print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfill(8)}.{bin(ip[3])[2:].zfill(8)}')
-
-
-ip = list(map(int, '93.138.64.0'.split('.')))
-print(f'{bin(ip[0])[2:].zfill(8)}.{bin(ip[1])[2:].zfill(8)}.{bin(ip[2])[2:].zfill(8)}.{bin(ip[3])[2:].zfill(8)}')
-
-
--- main.py
+++ main.py
@@ -1,32 +1,8 @@
-'''Рассмотрим шестнадцатеричные четырёхзначные числа. Сколько существует таких чисел, y которых:
+# from itertools import *
 
-1. B записи числа присутствует две чётные и две нечётные цифры.
-2. Сумма чётных цифр равна сумме нечётных цифр.
-3. Ни одна из цифр не повторяется.
-
-
-f = '278A'
-
-{2, 7, 8, A}
-'''
-ch_alph = '02468ACE'
-nch_alph = '13579BDF'
-c=0
-v=0
-for i1 in "123456789ABCDEF":
-    for i2 in "0123456789ABCDEF":
-        for i3 in "0123456789ABCDEF":
-            for i4 in "0123456789ABCDEF":
-                d=i1+i2+i3+i4
-                ch = [i for i in d if i in ch_alph] 
-                nch = [i for i in d if i in nch_alph]
-                if len(ch)==2:
-                    sm_ch = int(ch[0],16) + int(ch[1],16)
-                    sm_nch = int(nch[0],16)+int(nch[1],16)
-                    if sm_ch==sm_nch:
-                        if len(set(d))==4:
-                            c+=1
-print(c)
-
-
-
+# tq="235 145 146 2367 1267 345 45".split()
+# p="ED EC EF CF CE CG GC GB GA GD BF FA AD".split()
+# print(*range(1,8))
+# for var in permutations("EDCGBFA"):
+#     if all(str(var.index(x)+1) in tq[var.index(y)] for x, y in p):
+#         print(*var)
